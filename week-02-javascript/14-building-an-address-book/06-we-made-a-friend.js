d03a7cb90e776fe70ac27ff722fb079f3f419106
ehlojames <ehlojames@users.noreply.github.com>
2015-07-21 23:50:34 +0100

    Commit Building an Address Book

---
+++ week-02-javascript/14-building-an-address-book/06-we-made-a-friend.js
@@ -0,0 +1,48 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+};
+
+function list() {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+};
+
+function search(lastName) {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        if (contacts[i].lastName === lastName) {
+            printPerson(contacts[i]);
+        }
+    }
+};
+
+function add(firstName, lastName, email, phoneNumber) {
+    var person = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber
+    };
+    contacts[contacts.length] = person;
+};
+
+add("Esther", "Dobson", "esther.dobson@example.com", "(708) 798-3646");
+list();
