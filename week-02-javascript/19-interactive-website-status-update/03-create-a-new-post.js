ce3324a6a126a75f5cd95a0f6e14684343186566
ehlojames <ehlojames@users.noreply.github.com>
2015-07-22 21:12:49 +0100

    Commit Interactive Website: Status Update

---
+++ week-02-javascript/19-interactive-website-status-update/03-create-a-new-post.js
@@ -0,0 +1,7 @@
+var main = function() {
+    $('.btn').click(function() {
+        var post = $('.status-box').val();
+        $('<li>').text(post).prependTo('.posts');
+    });
+};
+$(document).ready(main);
